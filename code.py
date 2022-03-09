c2cb692465ff73cf50030f3ca3b8b55df97ac9e6
Maxime G <80921412+Bromorr@users.noreply.github.com>
2022-03-09 17:19:59 +0100

    Update code.py

--- code.py
+++ code.py
@@ -2,17 +2,17 @@ import numpy as np
 import numpy.random as rd
 import matplotlib.pyplot as plt
 
-def log_density(x, alpha, beta, tau, gamma, data):
+def log_dens_gamma(gamma, alpha, beta, data):
   #Data must be an array
   
     if tau <= 0 or alpha <= 1 or beta <= 1 or gamma < 0 or gamma > 1:
         return 0
   
     n = len(data)
-    dens = -n/2*np.log(tau) - n*alpha/2 - 1/(2*tau)*data[:,1].sum()
+    dens = 0
     
     for i in range(n):
-        dens += beta*gamma**data[i,0]
+        dens += np.log(data[i, 1] - alpha + beta*gamma**data[i, 0])
     
     return dens
 
