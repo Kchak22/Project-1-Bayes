677a8e67ab206d95ebdc91cf4f2633ba64e6fe70
SoufianEchabarri <96385012+SoufianEchabarri@users.noreply.github.com>
2022-03-09 17:45:40 +0100

    Update code.py

--- code.py
+++ code.py
@@ -3,12 +3,12 @@ import numpy.random as rd
 import matplotlib.pyplot as plt
 
 
-def log_dens_gamma(gamma, alpha, beta, data):
+def log_dens_gamma(gamma, alpha,beta, data):
     dens  = data[:, 1].sum() - alpha + beta*np.power(gamma, data[:, 0])[0]
     return dens
 
-  
-def GibbsSampler(nchain, initialisation, data) :
+ param_defaut=[mu_alpha, sigma_alpha, mu_beta, sigma_beta, mu_tau, sigma_beta,sigma]
+def GibbsSampler(nchain, initialisation, data, param=param_defaut) :
    ## nchain: taille de la chaine
    
    # Initialisation 
@@ -17,7 +17,7 @@ def GibbsSampler(nchain, initialisation, data) :
     
    for i in range(nchain):
     ## Mise a jour de alpha
-    chain[i+1,0] = np.random.normal(,)
+    chain[i+1,0] = np.random.normal((1/(1/param[1]**2)+n/param[6]**2)*(param[0]/param[1]**2),)
     ## Mise a jour de  Beta
     chain[i+1,1] = np.random.normal(, )
     ## Mise a jour de  Tau
@@ -26,8 +26,8 @@ def GibbsSampler(nchain, initialisation, data) :
  
         prop = chain[i,3] + 
         
-        top = log_dens_gamma(chain[i,3], chain[i,0], chain[i,1], chain[i,2])
-        bottom =log_dens_gamma(chain[i-1,3], chain[i-1,0], chain[i-1,1], chain[i-1,2])
+        top = 
+        bottom = 
         acc_prob = np.exp(top - bottom)
         
         if np.random.uniform() < acc_prob:
