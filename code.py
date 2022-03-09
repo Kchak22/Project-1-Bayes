885b8ae58dc6a6874e188da12513fcb21e43aa7c
Maxime G <80921412+Bromorr@users.noreply.github.com>
2022-03-09 15:27:54 +0100

    Changed indentation

--- code.py
+++ code.py
@@ -4,13 +4,13 @@ import numpy.random as rd
 def log_density(x, alpha, beta, tau, gamma, data):
   #Data must be an array
   
-  if tau <= 0 or alpha <= 1 or beta <= 1 or gamma < 0 or gamma > 1:
-    return 0
+    if tau <= 0 or alpha <= 1 or beta <= 1 or gamma < 0 or gamma > 1:
+        return 0
   
-  n = len(data)
-  dens = -n/2*np.log(tau) - n*alpha/2 - 1/(2*tau)*data[:,1].sum()
-  
-  for i in range(n):
-    dens += beta*gamma**data[i,0]
-  
-  return dens
+    n = len(data)
+    dens = -n/2*np.log(tau) - n*alpha/2 - 1/(2*tau)*data[:,1].sum()
+    
+    for i in range(n):
+        dens += beta*gamma**data[i,0]
+    
+    return dens
