8b68a4d897ec6131a2cda17fd1ad988a7602655f
SoufianEchabarri <96385012+SoufianEchabarri@users.noreply.github.com>
2022-03-09 17:56:05 +0100

    Update code.py

--- code.py
+++ code.py
@@ -7,7 +7,8 @@ def log_dens_gamma(gamma, alpha,beta, data):
     dens  = data[:, 1].sum() - alpha + beta*np.power(gamma, data[:, 0]).sum()
     return dens
 
- param_defaut=[mu_alpha, sigma_alpha, mu_beta, sigma_beta, mu_tau, sigma_beta,sigma]
+    sigma=1/sqrt(tau)
+    param_defaut=[mu_alpha, sigma_alpha, mu_beta, sigma_beta, mu_tau, sigma_beta,sigma]
 def GibbsSampler(nchain, initialisation, data, param=param_defaut) :
    ## nchain: taille de la chaine
    
@@ -17,7 +18,8 @@ def GibbsSampler(nchain, initialisation, data, param=param_defaut) :
     
    for i in range(nchain):
     ## Mise a jour de alpha
-    chain[i+1,0] = np.random.normal((1/(1/param[1]**2)+n/param[6]**2)*(param[0]/param[1]**2),)
+    chain[i+1,0] = np.random.normal((1/(1/param[1]**2)+n/param[6]**2)*(param[0]/param[1]**2+sum(data[:,1])/param[6]),\
+                                    1/(1/param[1]**2+n/param[6]))
     ## Mise a jour de  Beta
     chain[i+1,1] = np.random.normal(, )
     ## Mise a jour de  Tau
