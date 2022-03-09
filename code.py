ff76bf14a605446db6fa8fd9d79a98b9eae5b2ea
Maxime G <80921412+Bromorr@users.noreply.github.com>
2022-03-09 15:21:00 +0100

    Definition of the log posterior density

--- code.py
+++ code.py
@@ -2,4 +2,11 @@ import numpy as np
 import numpy.random as rd
 
 def log_density(x, alpha, beta, tau, gamma, data):
+  #Data must be an array
+  n = len(data)
+  dens = -n/2*np.log(tau) - n*alpha/2 - 1/(2*tau)*data[:,1].sum()
   
+  for i in range(n):
+    dens += gamma**data[i,0]
+  
+  return dens
